2cb51855e555174ebc060d2c471933ae070adb12
lucaruboni <lucaruboni@gmail.com>
2023-03-08 15:46:54 +0100

    [add] km age prompt
    [add] ticket price

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,30 @@
-alert("ciao")
+/* Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+
+
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+
+va applicato uno sconto del 20% per i minorenni
+
+va applicato uno sconto del 40% per gli over 65.
+
+L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). */
+
+
+
+/* chiedere quanti km vuole percorrere l'utente */
+
+const kilometers = prompt("How many km do you have to travel?");
+console.log(kilometers);
+
+const customerAge = prompt("What's your age?")
+console.log(customerAge);
+
+let ticketPrice = (Number(kilometers * 0.21))
+console.log(ticketPrice)
+
+if (customerAge <= 18) {
+
+    let ticketPrice = Math.
+}
+
